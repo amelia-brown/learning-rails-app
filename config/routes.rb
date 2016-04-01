63d1e93c79a725fa89075e8e73c92608de0e985d
Amelia Brown <amelia.brown389@gmail.com>
2016-04-01 01:14:39 -0400

    (feature): add header styles and user model

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'work' => 'static_pages#work'
   get 'contact' => 'static_pages#contact'
   get 'addUser' => 'users#new'
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

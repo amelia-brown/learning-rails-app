af7da564a341e790372d606cb805de11299e1fa6
Amelia Brown <amelia.brown389@gmail.com>
2016-04-03 22:54:22 -0400

    (feature): add log in/out

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get 'work' => 'static_pages#work'
   get 'contact' => 'static_pages#contact'
   get 'addUser' => 'users#new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
